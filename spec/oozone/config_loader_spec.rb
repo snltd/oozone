e585671adc1d0d1d734a0ef78081a6cdf3e70d53
Robert Fisher <services@id264.net>
2023-07-05 16:20:00 +0000

    run setup

--- spec/oozone/config_loader_spec.rb
+++ spec/oozone/config_loader_spec.rb
@@ -3,3 +3,11 @@
 
 require_relative '../spec_helper'
 require_relative '../../lib/oozone/config_loader'
+
+# Test the config loader via its public interface.
+#
+class ConfigLoaderTest < MiniTest::Test
+  def setup
+    Oozone::DatasetManager.any_instance.stubs(:create).returns(true)
+  end
+end
